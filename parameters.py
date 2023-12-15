ecfceb45305249d9a6299b568c1cde256cede8bb
Rohit Kumar <42992895+rohit-kumar1907@users.noreply.github.com>
2023-12-15 19:44:46 +0530

    Delete parameters.py

--- parameters.py
+++
@@ -1,4 +0,0 @@
-VOCAB_FILE = 'weights/biobert_v1.1_pubmed_dummy/vocab.txt'
-BERT_CONFIG_FILE = 'weights/biobert_v1.1_pubmed_dummy/bert_config.json'
-BC5CDR_WEIGHT = 'weights/bc5cdr_wt.pt'
-BERT_WEIGHTS = 'weights/pytorch_weight'
